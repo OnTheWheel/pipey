868d91792081633e2823281dd5f58f15a5c0cdf5
Kwangmin Choi <mhmckm@gmail.com>
2011-07-30 17:01:21 +0900

    invalid paths in exceptions.h were corrected.

--- src/common/exception/exceptions.h
+++ src/common/exception/exceptions.h
@@ -1,12 +1,12 @@
 #ifndef PIPEY_EXCEPTION_H
 #define PIPEY_EXCEPTION_H
 
-#include "./exception/OutOfBound.h"
-#include "./exception/InvalidParameter.h"
-#include "./exception/DivideByZero.h"
-#include "./exception/MemoryAllocation.h"
-#include "./exception/MemoryCorruption.h"
-#include "./exception/InvalidState.h"
-#include "./exception/Sync.h"
+#include "OutOfBound.h"
+#include "InvalidParameter.h"
+#include "DivideByZero.h"
+#include "MemoryAllocation.h"
+#include "MemoryCorruption.h"
+#include "InvalidState.h"
+#include "Sync.h"
 
 #endif
